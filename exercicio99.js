05dd637ff6fe1abbb8961f8e63e7c40effb9b955
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-02-17 12:58:47 -0300

    resolução de 95 a 100

---
+++ exercicio99.js
@@ -0,0 +1,8 @@
+//Adicione (push) os números ímpares de 9 até 1 para myArray usando um laço for.
+
+// Configuração
+const myArray = [];
+for (let i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
+// Altere apenas o código abaixo desta linha
