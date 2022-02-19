f8deba50fc119d0cf0ef04e920f9f2d434972c5e
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-02-19 20:41:12 -0300

    update 103/104

---
+++ exercicio104.js
@@ -0,0 +1,15 @@
+/*Pesquisar perfil
+Temos um array de objetos representando pessoas diferentes nas nossas listas de contatos.
+
+Uma função lookUpProfile, que recebe name e uma propriedade (prop) como argumentos, foi pré-escrita para você.
+
+A função deve verificar se name é o firstName (primeiro nome) de um contato e se a propriedade passada (prop) é uma propriedade daquele contato.
+
+Se ambos forem verdadeiros, então retorne o "valor" daquela propriedade.
+
+Se name não corresponder a nenhum dos contatos, então retorne a string No such contact.
+
+Se prop não corresponder a nenhuma propriedade válida de um contato encontrado para coincidir com name então retorne a string No such property.
+
+ */
+
