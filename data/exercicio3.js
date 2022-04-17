8fe73b1191f8f91da45dad18d83fa93bd8b66680
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-04-17 13:07:17 -0300

    Update exercicio3.js

---
+++ data/exercicio3.js
@@ -0,0 +1,1 @@
+//Definimos uma função, mixedNumbers, que estamos passando um array como argumento. Modifique a função usando push()e unshift()para adicionar 'I', 2, 'three'ao início da matriz e 7, 'VIII', 9ao final para que a matriz retornada contenha representações dos números de 1 a 9 em ordem.
