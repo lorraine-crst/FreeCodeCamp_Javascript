3621a6a2e6ba4cbb85d02bb5643b53db82839ffa
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-04-17 13:03:41 -0300

    basic data structures exercites

---
+++ data/exercicio1.js
@@ -0,0 +1,3 @@
+//Definimos uma variável chamada yourArray. Complete a instrução atribuindo uma matriz de pelo menos 5 elementos de comprimento à yourArrayvariável. Sua matriz deve conter pelo menos uma string , um número e um booleano .
+
+let yourArray = ['one', 2, 'three', true, false, undefined, null]; // Change this line
