b9cbf9c66c4302304b091676f89d5781576ea9e9
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-04-17 13:07:22 -0300

    Update exercicio2.js

---
+++ data/exercicio2.js
@@ -0,0 +1,7 @@
+//Para completar este desafio, defina a 2ª posição (índice 1) de myArrayqualquer coisa que você queira, além da letra b.
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "l";
+// Only change code above this line
+console.log(myArray);
