e208f5b9393511987d94efd695dd870e38dede7a
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-04-19 15:29:18 -0300

    Update exercicio3.js

---
+++ data/exercicio3.js
@@ -0,0 +1,21 @@
+ // Verifique se as seguintes variáveis são Truthy ou Falsy
+ var nome = 'Andre';
+ var idade = 28;
+ var possuiDoutorado = false;
+ var empregoFuturo;
+ var dinheiroNaConta = 0;
+ // Compare o total de habitantes do Brasil com China (valor em milhões)
+ var brasil = 207;
+ var china = 1340;
+
+ // 0 que irá aparecer no console?
+ if (('Gato' === 'gato') && (5 > 2)) {
+     console.log('Verdadeiro');
+ } else {
+     console.log('Falso');
+     // 0 que irá aparecer no console?
+     if (('Gato' === 'gato') || (5 > 2)) {
+         console.log('Gato' && 'Cão');
+     } else 1
+     console.log('Falso');
+ }
